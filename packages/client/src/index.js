1ed0d703ea78ae4bea244331584bc31e35a0c016
linearza <vanwijk.mc@gmail.com>
2023-04-11 11:30:20 +0200

    feat: add button to ping server, with api endpoint to respond and CORS allow all rule

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -2,9 +2,27 @@ import m from 'mithril'
 
 const root = document.getElementById('app')
 
+const config = {
+  apiUrl: 'http://localhost:3000/api'
+}
+
 const App = {
+  value: '',
+  fetchValue: () => {
+    m.request({
+      method: 'GET',
+      url: `${config.apiUrl}/value`
+    })
+    .then(response => {
+      App.value = response.value
+    })
+  },
   view: () => {
-    return m('h1', 'Hello Mithril!')
+    return m('div', [
+      m('h1', 'Hello Mithril!'),
+      m('button', { onclick: App.fetchValue }, 'Ping API'),
+      App.value && m('p', App.value)
+    ])
   }
 }
 
--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/api/value', (req, res) => {
+  res.json({ value: 'Hello from the server!' })
 })
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000')
 })
+
